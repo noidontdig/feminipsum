d4995bac04b0c0cf5c1d073e8037b1ef1db80f00
erinzobitz <erinzobitz@gmail.com>
2015-12-19 17:49:01 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var paragraphLength = Math.random() * (150 - 60) + 60;
+var sentenceLength = Math.random() * (20 - 3) + 3;
+
+var loremIpsum = [];
+
+var feministWords= [];
+
+generateFeminipsum(n) {
+    // TODO
+}
