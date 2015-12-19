31d5626483e0f877731a2333fb58ce64d48b0e37
erinzobitz <erinzobitz@gmail.com>
2015-12-19 18:30:52 -0500

    Generate feminipsum

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-var paragraphLength = Math.random() * (150 - 60) + 60;
-var sentenceLength = Math.random() * (20 - 3) + 3;
+const SENTENCES_IN_PARA = 5;
 
-var loremIpsum = [];
+var sentenceLength = Math.random() * (15 - 3) + 3;
+var ratio = Math.random() * (3 - 1) + 1;
+
+var loremIpsum= [];
 
 var feministWords= [];
 
 generateFeminipsum(n) {
-    // TODO
+    var text;
+    _.each(n, function() {
+        text += generateParagraph();
+    });
+
+    return text;
+}
+
+generateParagraph() {
+    var paragraph;
+    _.each(SENTENCES_IN_PARA, function() {
+        paragraph += generateSentence() + "\n";
+    });
+
+    return paragraph;
+}
+
+generateSentence() {
+    var loremWordsArray = _.sample(loremIpsum, sentenceLength);
+    var femWordsArray = _.sample(feministWords, sentenceLength);
+
+    var feminipsumArray = _.shuffle(_.flatten([femWordsArray, loremWordsArray]));
+
+    return feminipsumArray.join(" ") + ".";
 }
